a8476f075ec534012801b0b4506e392181f5b9b1
wesam-c <wesameldeeb15@gmail.com>
2024-05-24 17:33:17 +0400

    updated index file - task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ Index
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route("/status", methods=['GET'], strict_slashes=False)
 def status():
@@ -15,6 +16,22 @@ def status():
         "status": "OK"
     }
 
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of each object by type"""
+    resp = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
     resp = jsonify(data)
     resp.status_code = 200
 
