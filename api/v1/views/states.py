8296fe11ecfb3e8fcdbce825021cdb7dfc35db21
wesam-c <wesameldeeb15@gmail.com>
2024-05-26 21:42:50 +0400

    Update states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,15 +1,45 @@
 #!/usr/bin/python3
-"""Blueprint"""
+"""states"""
 
-from flask import Blueprint
+from flask import Flask, jsonify, make_response, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from api.v1.views.index import *
-from models import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+@app_views.route('/states', methods=['GET'])
+def get_state():
+    """get list of states"""
+    states = [obj.to_dict() for obj in storage.all(State).values()]
+    return make_response(jsonify(states), 200)
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state_by_id(state_id):
+    """get state by id"""
+    state = storage.get(State, state_id)
+    return make_response(jsonify(state.to_dict()),
+                         200) if state else abort(404)
+
+@app_views.route('/states/<state_id>', methods=['POST'])
+def create_state():
+    """create a new state"""
+    state = request.get_json()
+    if not state:
+        return make_response("Not a JSON", 400)
+    if not state.get('name'):
+        return make_response("Missing name", 400)
+    new_state = State(**state)
+    new_state.save()
+    return make_response(jsonify(new_state.to_dict()), 201)
+
+@app_views.route('/states/<state_id>', methods=['PUT'])
+def update_state(state_id):
+    """update a state by id"""
+    cur_state = storage.get(State, state_id)
+    if not cur_state:
+        abort(404)
+    new_state = request.get_json()
+    if not new_state:
+        return make_response("Not a JSON", 400)
+    setattr(cur_state, 'name', new_state.get('name'))
+    storage.save()
+    return make_response(cur_state.to_dict(), 200)
