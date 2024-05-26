0efeafc623ddcd2dd6cde97fe1dab714e3c0a280
wesam-c <wesameldeeb15@gmail.com>
2024-05-26 20:31:12 +0400

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,8 @@
 APP
 """
 
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 import os
 from models import storage
 from api.v1.views import app_views
@@ -16,6 +17,21 @@ app.register_blueprint(app_views)
 def teardown(exception):
     """Function that closes the storage on teardown"""
     storage.close()
+
+@app.errorhandler(404)
+def handle_404(exception):
+    """
+    handles 404 error
+    :return: returns 404 json
+    """
+    data = {
+        "error": "Not found"
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 404
+
+    return(resp)
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
